af1e782798a6851ae1356f7a74ee95b05b9f5232
And-rob-2000 <acr3000@yahoo.com>
2021-02-07 18:49:35 -0600

    Create enemyTest.py
    Started writing for enemy movement

---
+++ enemyTest.py
@@ -0,0 +1,10 @@
+import pygame  
+pygame.init()
+
+pygame.display.set_caption("enemy test")
+
+
+enemyR = [pygame.image.load('R1E.png'),pygame.image.load('R2E.png'),pygame.image.load('R3E.png'),pygame.image.load('R4E.png'),pygame.image.load('R5E.png'),pygame.image.load('R6E.png'),pygame.image.load('R7E.png'),pygame.image.load('R8E.png')pygame.image.load('R9E.png')]
+enemyL = [pygame.image.load('L1E.png'),pygame.image.load('L2E.png'),pygame.image.load('L3E.png'),pygame.image.load('L4E.png'),pygame.image.load('L5E.png'),pygame.image.load('L6E.png'),pygame.image.load('L7E.png'),pygame.image.load('L8E.png')pygame.image.load('L9E.png')]
+enemyRattack = [pygame.image.load('R10E.png'),pygame.image.load('R11E.png')] 
+enemyLattack = [pygame.image.load('L10E.png'),pygame.image.load('L11E.png')]
